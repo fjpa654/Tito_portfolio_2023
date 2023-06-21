d9f515317e72020b5681d243162406fd82f65d09
fjpa654 <fjpa654@users.noreply.github.com>
2023-06-21 10:17:12 -0600

    wrote template for the data js file

---
+++ portofolio-website/src/portofolioData.js
@@ -0,0 +1,30 @@
+export const portfolioData = {
+    personalInfo: {
+        name: 'John Doe',
+        bio: 'Software Engineer with over 10 years of experience...',
+        links: {
+            projects: 'http://example.com/projects',
+            socialMedia: 'http://example.com/socialMedia',
+            clients: 'http://example.com/clients',
+        },
+    },
+    events: [
+        {
+            startDate: '2022-01-01',
+            endDate: '2023-12-31',
+            achievement: 'Master of Computer Science',
+            type: 'academic',
+            description: 'Master degree in Computer Science with a focus on AI',
+            entityName: 'MIT',
+        },
+        {
+            startDate: '2024-01-01',
+            endDate: '2025-12-31',
+            achievement: 'Software Development Project',
+            type: 'professional',
+            description: 'Developed an ecommerce platform for a well-known client',
+            entityName: 'XYZ Co.',
+        },
+        // more events...
+    ],
+};
